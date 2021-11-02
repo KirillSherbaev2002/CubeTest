f132625be4f2159525b423843aee6997d43a0435
Kirill2002 <KSherbaev2002@gmail.com>
2021-11-02 23:36:28 +0300

    WHatPOssible

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+using Dreamteck.Splines;
+using UnityEditor.ShaderGraph.Internal;
+using UnityEngine.PlayerLoop;
+
+public class GameController : MonoBehaviour
+{
+    [Header("Input")]
+    [SerializeField] private Slider coreForceSlider;
+    [SerializeField] private GameObject sliderHandle;
+    
+    [Header("Objects")]
+    [SerializeField] private GameObject core;
+    [SerializeField] private GameObject movingPartOfCannon;
+    [SerializeField] private GameObject cannon;
+    [SerializeField] private GameObject aim;
+    [SerializeField] private Camera cam;
+    [SerializeField] private GameObject coreInstantiator;
+    [SerializeField] private GameObject explosion;
+    [SerializeField] private GameObject splineCore;
+
+    [Header("Tracking")]
+    private Vector3[] trajectory;
+    [SerializeField] private float trackingFrequency;
+    [SerializeField] private float valueToStop;
+    [SerializeField] private float forceToAdd;
+    [SerializeField] private float distanceToSkipShot;
+    public float timeToWait;
+
+    private Vector3 aimToWorld;
+    private GameObject coreCreated;
+    private SplineFollower coreCreatedFollower;
+    private float sliderValue;
+    private bool isControlledBySlider;
+
+    public Action< int > trackingCall;
+    public Action< float> onSliderControlGoing;
+    public Action onMovementEnded;
+   
+    
+    private void Awake()
+    {
+        SetToDefault();
+        trackingCall += SetTrajectoryOfSphere;
+        onSliderControlGoing += OnShotCompleted;
+    }
+    
+    private void SetToDefault()
+    {
+        splineCore.AddComponent<SplineComputer>();
+        splineCore.AddComponent<SplineRenderer>();
+        coreForceSlider.interactable = false; 
+    }
+
+    private void OnShotCompleted(float value2)
+    {
+        sliderHandle.SetActive(true);
+    }
+    private void OnMouseDrag()
+    {
+        if (Input.mousePosition.x <= Screen.height / 15f )
+            return;
+
+        aim.SetActive(true);
+        //Aim activation
+            
+        Vector3 localTouch = Input.mousePosition;
+        localTouch.z = 23f;
+        //Needed because Z means distance for Quaternion.LookRotation
+            
+        aim.GetComponent<RectTransform>().position = new Vector3(localTouch.x, localTouch.y);
+        aimToWorld = cam.ScreenToWorldPoint(localTouch);
+        movingPartOfCannon.transform.rotation = Quaternion.LookRotation(aimToWorld);
+        //Set needed values to aim and movingPartOfCannon
+    }
+
+    private void OnMouseUp()
+    {
+        DestoryPreviousBall();
+        Shot();
+    }
+
+    private void DestoryPreviousBall()
+    {
+        GameObject previousBall = GameObject.FindGameObjectWithTag("Core");
+        Destroy(previousBall);
+    }
+
+    private void Shot()
+    {
+        var spawnPosition = coreInstantiator.transform.position;
+        var spawnRotation = coreInstantiator.transform.rotation;
+        
+        coreCreated = Instantiate(core, spawnPosition, spawnRotation);
+        Instantiate(explosion, spawnPosition, spawnRotation);
+        
+        coreCreated.GetComponent<Rigidbody>().AddForce(aimToWorld * forceToAdd);
+        
+        StartCoroutine(ControllerLaunched(0));
+    }
+
+    private void FixedUpdate()
+    {
+        if (isControlledBySlider)
+        {
+            coreCreatedFollower.startPosition = coreForceSlider.value;
+            onSliderControlGoing( coreForceSlider.value);
+        }
+    }
+
+    IEnumerator ControllerLaunched(int i)
+    {
+        yield return new WaitForSeconds(0.03f);
+        bool isBallPhysical = true;
+        float timeScore = 0;
+
+        while (isBallPhysical)
+        {
+            i++;
+            trackingCall(i);
+            yield return new WaitForSeconds(0.3f / coreCreated.GetComponent<Rigidbody>().velocity.magnitude);
+            print(0.1f / coreCreated.GetComponent<Rigidbody>().velocity.magnitude);
+
+            timeScore += 3 * Time.deltaTime;
+
+            print(timeScore);
+            if (timeScore >= timeToWait)
+            {
+                isBallPhysical = false;
+            }
+        }
+
+        onMovementEnded();
+        coreCreated.GetComponent<Collider>().isTrigger = true;
+        coreCreated.GetComponent<Rigidbody>().isKinematic = true;
+        coreCreated.AddComponent<SplineFollower>().spline = splineCore.GetComponent<SplineComputer>();
+        coreCreatedFollower = coreCreated.GetComponent<SplineFollower>();
+        coreCreatedFollower.follow = false;
+        coreCreatedFollower.physicsMode = SplineTracer.PhysicsMode.Transform;
+        coreForceSlider.interactable = true;
+        isControlledBySlider = true;
+    }
+
+    // private void GetTrajectoryOfSphere(int valueInArray, float fillSlider)
+    // {
+    //     if (sliderValue != coreForceSlider.value)
+    //     {
+    //         sliderValue = coreForceSlider.value;
+    //         coreCreated.GetComponent<SplineFollower>().startPosition = fillSlider;
+    //     }
+    // }
+
+    private void SetTrajectoryOfSphere(int valueInArray)
+    {
+        splineCore.GetComponent<SplineComputer>().SetPointPosition(valueInArray, coreCreated.transform.position);
+    }
+}
---
+++ Assets/Scripts/StoneScript.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Dreamteck.Splines;
+
+
+public class StoneScript : MonoBehaviour
+{
+    private Vector3[] objectTrajectory;
+    private float quantityOfTracking;
+    private GameController gameController;
+    [SerializeField] private float distanceToNotCount; 
+    private Quaternion defaultQuaternion;
+    [SerializeField] private Quaternion finalQuaternion;
+    private GameObject child;
+    private SplineFollower coreCreatedFollower;
+    private SplineComputer itsSplineComputer;
+    
+    private List<Quaternion> stoneRotationValue = new List<Quaternion>();
+    private void Awake()
+    {
+        gameController = GameObject.FindObjectOfType<GameController>();
+        gameController.trackingCall += SetTrajectory;
+        gameController.onSliderControlGoing += GetTrajectory;
+        gameController.onMovementEnded += OnCollision;
+        gameController.onMovementEnded += SetRotationFinal;
+
+        gameObject.AddComponent<SplineComputer>();
+        gameObject.AddComponent<SplineRenderer>();
+        itsSplineComputer = GetComponent<SplineComputer>();
+        print("SetTrajectory");
+        child = transform.GetChild(0).gameObject;
+        
+        defaultQuaternion = child.transform.rotation;
+    }
+
+    private void OnCollision()
+    { 
+        child.AddComponent<SplineFollower>().spline = itsSplineComputer;
+        child.GetComponent<SplineFollower>().follow = false;
+        child.GetComponent<Collider>().isTrigger = true;
+        
+        child.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionX;
+        child.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionY;
+        child.GetComponent<Rigidbody>().isKinematic = true;
+        
+        coreCreatedFollower = child.GetComponent<SplineFollower>();
+        coreCreatedFollower.follow = false;
+        coreCreatedFollower.motion.applyRotation = false;
+        coreCreatedFollower.physicsMode = SplineTracer.PhysicsMode.Transform;
+    }
+
+    private void SetTrajectory(int valueInArray)
+    {
+        int pointCount = itsSplineComputer.pointCount;
+        if (pointCount == 0) 
+        {
+            itsSplineComputer.SetPointPosition(pointCount, child.transform.position);
+            stoneRotationValue.Add(child.transform.rotation);
+            return;
+        }
+
+        if (Vector3.Distance(itsSplineComputer.GetPointPosition(pointCount - 1),
+            child.transform.position) > distanceToNotCount)
+        {
+            itsSplineComputer.SetPointPosition(pointCount, child.transform.position);
+            stoneRotationValue[valueInArray] = child.transform.rotation;
+        }
+    }
+
+    private void SetRotationFinal()
+    {
+        finalQuaternion = child.transform.rotation;
+    }
+
+    private void GetTransitionRotation(Quaternion firstRotation,Quaternion secondRotation, float fill)
+    {
+        Quaternion neededQuaternionByFill = new Quaternion((firstRotation.x * (1-fill)) + (secondRotation.x * fill),
+            (firstRotation.y * (1-fill)) + (secondRotation.y* fill),
+            (firstRotation.z * (1-fill)) + (secondRotation.z* fill),0);
+        transform.rotation = neededQuaternionByFill;
+    }
+
+    private void GetTrajectory(float fill)
+    {
+        child.GetComponent<SplineFollower>().startPosition = fill;
+        //GetTransitionRotation();
+    }
+    
+    private void GetRotationNeeded(float fill)
+    {
+        if (finalQuaternion == new Quaternion(0,0,0,0))
+        {
+            return;
+        }
+        Quaternion neededQuaternionByFill = new Quaternion((defaultQuaternion.x * (1-fill)) + (finalQuaternion.x * fill),
+            (defaultQuaternion.y * (1-fill)) + (finalQuaternion.y* fill),
+            (defaultQuaternion.z * (1-fill)) + (finalQuaternion.z* fill),0);
+        print(neededQuaternionByFill);
+        
+        child.transform.rotation = Quaternion.EulerAngles(neededQuaternionByFill.x,neededQuaternionByFill.y,neededQuaternionByFill.z);
+    }
+
+}
